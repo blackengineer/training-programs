fbf295efd315657d7b1334f554c8b1fbb382d4fd
Jimmy <jimmlindsey@gmail.com>
2024-01-25 04:32:34 -0800

    add teacher analytics dashboard

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,7 +1,38 @@
-const AnalyticsPage = () => {
+import { getAnalytics } from '@/actions/get-analytics'
+import { redirect } from 'next/navigation'
+import { DataCard } from './_components/DataCard'
+import Chart from './_components/Chart'
+import { currentUser } from '@/lib/auth'
+
+const AnalyticsPage = async () => {
+  const user = await currentUser()
+
+  if (user?.role !== 'ADMIN') {
+    return redirect("/dashboard")
+  }
+
+  const {
+    data,
+    totalRevenue,
+    totalSales,
+  } = await getAnalytics(user.id)
+
   return (
-    <div className='p-4'>
-      Analytics page
+    <div className='p-6'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-4 mb-4'>
+        <DataCard 
+          label='Total Revenue'
+          value={totalRevenue}
+          shouldFormat
+        />
+        <DataCard 
+          label='Total Sales'
+          value={totalSales}
+        />
+      </div>
+      <Chart 
+        data={data}
+      />
     </div>
   )
 }
