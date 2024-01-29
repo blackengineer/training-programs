aa2ad7fb9ef3d168d60b761960e5d38573798980
Jimmy <jimmlindsey@gmail.com>
2024-01-28 17:58:23 -0800

    add terms copy

--- app/(static)/terms/page.tsx
+++ app/(static)/terms/page.tsx
@@ -4,8 +4,304 @@ const Terms = () => {
   return (
     <div className='mt-20'>
       <h1 className='pl-6 pr-6 text-2xl font-bold'>
-        Terms
+        Terms and Conditions
       </h1>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        By using this website https://www.howtotrainfor.com (&quot;Website&quot;), You
+        are deemed to have read and agreed to the following terms and
+        conditions:
+      </p>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        The following terminology applies to these Terms and Conditions
+        (Terms), Privacy Policy and any disclaimer notice and any or all
+        Agreements on this Website:
+      </p>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        &quot;Customer&quot;, &quot;You&quot;, &quot;User&quot; and &quot;Your&quot; refers to you, the person
+        accessing this Website and exploiting Our Services/Products and
+        therefore accepting the Company&apos;s terms and conditions and;
+      </p>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        &quot;User Account&quot; or &quot;Account&quot; shall mean the online account maintained
+        by the User at the Website to avail of the Services;
+      </p>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        the &quot;Company&quot;, &quot;Our&quot;, &quot;We&quot; and &quot;Us&quot;, refers to Our Company, Muscle
+        Development Platforms.
+      </p>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        &quot;Party&quot; or &quot;Parties&quot; refers to both the Customer/User and Ourselves,
+        or either the Customer or Ourselves. &quot;Product&quot; refers to a unique
+        product provided by our Company as a result of Our commitment and
+        services (&quot;Services&quot;).
+      </p>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        Any use or access by anyone under the age of 16 is prohibited unless
+        permitted by the laws of Your country of residence, and certain regions
+        and/or Products may have additional requirements and/or restrictions.
+      </p>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        All rights not expressly granted to You under these Terms are reserved
+        to the Company.
+      </p>
+      <h2 className='pl-6 pr-6 text-base uppercase font-semibold mt-6'>
+        SERVICES/PRODUCT(S)
+      </h2>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        The Website allows You to purchase an online-course (Product) for a
+        fee. We may at Our sole discretion engage third party service
+        providers from time to time to provide certain Services. Each such
+        purchase is intended to be used only by one person, unless specified
+        otherwise under Product&apos;s details. You are not allowed to share Your
+        purchase and/or provide access to such purchase to a third party.
+        Please acknowledge that the Products we provide You with for a fee
+        paid through Our Website constitute Our intellectual property and may
+        not be exploited in any way prohibited by these Terms.
+      </p>
+      <h2 className='pl-6 pr-6 text-base uppercase font-semibold mt-6'>
+        LICENSE
+      </h2>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        Subject to these Terms and Our policies, We grant you a limited,
+        personal, non-exclusive, non-transferable, and revocable license to
+        use Our Services/Products. You may use Our Product(s) only for Your
+        personal, non-commercial use, unless You obtain Our written
+        permission to otherwise use the Product(s). You also agree that You
+        will access, and/or use only one User Account, unless expressly
+        permitted by Us, and You will not share access to Your User Account
+        or access information for Your Account with any third party. Using our
+        Services/Product(s) does not give you the ownership of or any
+        intellectual property rights in Our Services or the Product(s) you
+        access for a fee paid through Our Website.
+      </p>
+      <h2 className='pl-6 pr-6 text-base uppercase font-semibold mt-6'>
+        PRIVACY
+      </h2>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        We are committed to protecting Your privacy. We will only use
+        information collected from individual Customers particularly email
+        addresses to facilitate and deliver orders as part of Our commitment to
+        provide the Services Our Customers have paid. Moreover, as We
+        endeavor to provide You better products/services We will also be
+        using Your emails to promote Our other products/services. You can
+        opt out anytime by pressing the unsubscribe button that can be found
+        in every email We send. We will constantly review Our systems and
+        data to ensure the best possible service to Our Customers.
+      </p>
+      <h2 className='pl-6 pr-6 text-base uppercase font-semibold mt-6'>
+        SUPPORT
+      </h2>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        We will provide Customer support services to Our registered Users via
+        electronic mail. If You experience any difficulties using Our Product
+        please notify us using the following email: jimmy@howtotrainfor.com
+        with a subject line &quot;Support&quot;.
+      </p>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        However, We will not be liable or in any way responsible for Your own
+        technical issues, internet speed and other related to Your
+        access/device/location matters and We reserve the right to solely
+        determine whether such difficulty exists on Our side.
+      </p>
+      <h2 className='pl-6 pr-6 text-base uppercase font-semibold mt-6'>
+        DISCLAIMER
+      </h2>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        The information on this Website is provided on an &quot;AS IS&quot; basis. To
+        the fullest extent permitted by law, this Company: (i) excludes all
+        representations and warranties relating to this Website and its content
+        or which is or may be provided by any affiliates or any other third
+        party, including in relation to any inaccuracies or omissions in this
+        Website and/or the Company&apos;s Products; (ii) excludes all liability for
+        damages arising out of or in connection with Your use of this Website.
+        This includes, without limitation, direct loss, loss of business or profits
+        (whether or not the loss of such profits was foreseeable, arose in the
+        normal Product of things or You have advised this Company of the
+        possibility of such potential loss), damage caused to Your computer,
+        computer software, systems and programs and the data thereon or
+        any other direct or indirect, consequential and incidental damages.
+      </p>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        WITHOUT LIMITING THE FOREGOING, WE DO NOT WARRANT
+        THAT (A) THE PRODUCT(S) WILL MEET YOUR REQUIREMENTS
+        OR EXPECTATIONS OR ACHIEVE THE INTENDED PURPOSES, (B)
+        THE PRODUCTS WILL NOT EXPERIENCE OUTAGES OR
+        OTHERWISE BE UNINTERRUPTED, TIMELY, SECURE OR ERRORFREE, (C) THAT DEFECTS ON THE PRODUCTS WILL BE
+        CORRECTED. YOU ASSUME ALL RISK OF PERSONAL INJURY,
+        INCLUDING DEATH AND DAMAGE TO PERSONAL PROPERTY,
+        SUSTAINED FROM USE OF THE PRODUCT(S).
+      </p>
+      <h2 className='pl-6 pr-6 text-base uppercase font-semibold mt-6'>
+        FEES
+      </h2>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        Purchasing a Product:
+      </p>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        We offer paid Product(s) for a fee. You are responsible for paying all
+        fees charged by Us and applicable taxes in a timely manner with a
+        payment mechanism associated with the applicable paid Product(s).
+        When you make a purchase, you agree not to use an invalid or
+        unauthorized payment method. We reserve the right to disable access
+        to any Product for which we have not received adequate payment.
+        Unless otherwise stated, all fees are quoted in U.S. Dollars. You are
+        solely responsible for any sales, value-added, withholding or similar
+        taxes that apply to Your purchase, whether domestic or foreign.
+      </p>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        You can purchase Our Product(s) via credit card:
+        If You choose to pay by credit card You authorize Us to charge Your
+        credit card or bank account for an amount of Product&apos;s applicable fee.
+      </p>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        By purchasing any Product through Our Website, You hereby consent
+        and agree to abide by such third-party Internet payment service
+        providers&apos; customer terms and conditions, and privacy policies, and
+        understand that we have no control whatsoever on such customer
+        terms and conditions, and privacy policies. <span className='font-semibold'>IF You DO NOT AGREE
+        to such third-party Internet payment service providers&apos; customer
+        terms and conditions or privacy policies, DO NOT PURCHASE
+        ANY PRODUCT. This is Your sole responsibility to find, read and
+        understand any third party policies.</span>
+      </p>
+      <h2 className='pl-6 pr-6 text-base uppercase font-semibold mt-6'>
+        CUSTOMER RESTRICTIONS
+      </h2>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        As a Customer, You agree not to:
+      </p>
+      <ul className='pl-16 pr-6 text-sm mt-4 list-disc space-y-2'>
+        <li>Circumvent, disable, or otherwise interfere with security-related features of the Website;</li>
+        <li>Disparage, tarnish, or otherwise harm, in Our opinion, Website and/or the Service;</li>
+        <li>Use the Product in a manner inconsistent with any applicable laws or regulations;</li>
+        <li>Interfere with, disrupt, or create an undue burden on the Service or the networks or services connected to the Service;</li>
+        <li>You will not share access to Our Product with a third party (unless otherwise permitted by us separately in writing); will not lease, sell, grant or otherwise exploit Our Product(s) in a way prohibited by these Terms.</li>
+        <li>Attempt to bypass any measures of the Website designed to prevent or restrict access to the Service, or any portion of the Website.</li>
+      </ul>
+      <h2 className='pl-6 pr-6 text-base uppercase font-semibold mt-6'>
+        USER CONTENT AND CONDUCT
+      </h2>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        In case Our Services enable You to share your content (&quot;User
+        Content&quot;), You retain all intellectual property rights in, and are
+        responsible for, the User Content You create and share. However, by
+        submitting User Content you grant other Users and Company the right
+        to share Your User Content via any social media platform.
+      </p>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        As a user of the Site, you agree not to post any Prohibited Content:
+      </p>
+      <ol className='pl-16 pr-6 text-sm mt-4 list-decimal space-y-2'>
+        <li>Profane language or content;</li>
+        <li>Content that promotes, fosters, or perpetuates discrimination on the basis of race, religion, gender, marital status, familial status, national origin, age, mental or physical disability, sexual orientation, gender identity, source of income or other protected status under applicable law;</li>
+        <li> Inappropriate sexual content or links to inappropriate sexual content, nudity or obscene;</li>
+        <li>Conduct or encouragement of illegal activity;</li>
+        <li>Private and confidential information;</li>
+        <li>Content that violates a legal ownership interest of any other party.</li>
+      </ol>
+      <h2 className='pl-6 pr-6 text-base uppercase font-semibold mt-6'>
+        INTELLECTUAL PROPERTY OF OUR COMPANY
+      </h2>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        Unless otherwise indicated, this Website is Our proprietary property
+        and all source code, databases, functionality, software, Website
+        designs, audio, video, text, photographs, and graphics on the Website
+        (collectively, the &quot;Content&quot;) and the trademarks, service marks, and
+        logos contained therein (the &quot;Marks&quot;) are owned or controlled by Us or
+        licensed to Us, and are protected by copyright and trademark laws
+        and various other intellectual property rights and unfair competition
+        laws in various jurisdictions including EU and US, international
+        copyright laws, and international conventions.
+      </p>
+      <h2 className='pl-6 pr-6 text-base uppercase font-semibold mt-6'>
+        TERM AND TERMINATION
+      </h2>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        The Company may terminate your access to all or any part of the
+        Website, Services and/or Product(s) at any time, with or without
+        cause, with or without notice, effective immediately.
+      </p>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        You can stop using our Services at any time. You may delete your
+        User Account by Website features or notifying Us at
+        jimmy@howtotrainfor.com. When you decide to delete Your User
+        Account, We will delete Your data, although this may not take place
+        immediately.
+      </p>
+      <h2 className='pl-6 pr-6 text-base uppercase font-semibold mt-6'>
+        INDEMNITY
+      </h2>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        You agree to indemnify, defend and hold harmless Company and its
+        affiliates, and their respective licensors and Service Providers, and all
+        officers, directors, owners, agents, or licensors of any of the foregoing
+        (collectively, the &quot;Indemnified Parties&quot;) from and against any and all
+        losses, damages, liabilities and costs, including reasonable Attorney&apos;
+        fees, sustained by any of the Indemnified Parties in connection with
+        any claim arising out of Your use of Our Website or Product(s), or any
+        breach by You of these Terms. Under no circumstances shall We be
+        liable for any amount exceeding the fee paid for the
+        Services/Product(s) ordered.
+      </p>
+      <h2 className='pl-6 pr-6 text-base uppercase font-semibold mt-6'>
+        CHANGES TO THESE TERMS
+      </h2>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        We reserve the right to revise the Terms at our sole discretion at any
+        time. Any revisions to the Terms will be effective immediately upon
+        posting by Us. For any material changes to the Terms, we will take
+        reasonable steps to notify you of such changes, via a banner on the
+        website, email notification, another method, or combination of
+        methods. In all cases, your continued use of the Services/Products
+        after publication of such changes, with or without notification,
+        constitutes binding acceptance of the revised Terms.
+      </p>
+      <h2 className='pl-6 pr-6 text-base uppercase font-semibold mt-6'>
+        SEVERABILITY
+      </h2>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        If it turns out that a particular provision of these Terms is not
+        enforceable, this will not affect any other terms. If you do not comply
+        with these Terms, and we do not take immediate action, this does not
+        indicate that we relinquish any rights that we may have in accordance
+        with these Terms or granted by law.
+      </p>
+      <h2 className='pl-6 pr-6 text-base uppercase font-semibold mt-6'>
+        APPLICABLE LAW AND JURISDICTION
+      </h2>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        These Terms and Conditions are governed by the laws of US, CA the
+        parties submit to the jurisdiction of the Court of US, CA regardless of
+        conflicts of law rules, and Santa Monica Courts competent to hear
+        appeals from them.
+      </p>
+      <h2 className='pl-6 pr-6 text-base uppercase font-semibold mt-6'>
+        ENTIRE AGREEMENT
+      </h2>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        The Terms constitute the sole and entire agreement between You and
+        Us regarding this Website and supersede all prior and
+        contemporaneous understandings, agreements, representations, and
+        warranties, both written and oral, regarding the
+        Services/Product(s)/Website.
+      </p>
+      <h2 className='pl-6 pr-6 text-base uppercase font-semibold mt-6'>
+      CONTACTING US
+      </h2>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        For questions or clarifications regarding Our Terms and/or any other
+        matters related to Our Services/Products, please contact Us at
+        jimmy@howtotrainfor.com.
+      </p>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        Our mailing address is:
+      </p>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        Attn: Jimmy Lindsey
+      </p>
+      <p className='pl-6 pr-6 text-sm mt-4'>
+        Address: 3019 Ocean Park Blvd, STE 224, Santa Monica, CA, US 90405
+      </p>
       <div className='mt-20'>
         <Footer />
       </div>
