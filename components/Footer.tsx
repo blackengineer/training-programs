c9f3ce8ed2f88dcf29212d51d2b20df81180a1d3
Jimmy <jimmlindsey@gmail.com>
2024-01-31 10:51:08 -0800

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,31 +2,24 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div className='border-t flex justify-between p-6 text-xs text-muted-foreground'>
-      <div>
+    <div className='flex justify-between border-t p-6 text-xs text-muted-foreground'>
+      <div className='mb-2'>
         &copy; 2024 Muscle Development Platforms
       </div>
-      <div className='grid md:grid-cols-3'>
-        <div>
-          <Link
-            href={'/about'}>
-            About
-          </Link>
-        </div>
-        <div>
-          <Link 
-            className='mr-6'
-            href={'/terms'}>
-            Terms of Service
-          </Link>
-        </div>
-        <div>
-          <Link href={'/privacy'}>
-            Privacy Policy
-          </Link>
-        </div>
+      <div className='grid grid-cols-1 space-y-1'>
+        <Link
+          href={'/about'}>
+          About
+        </Link>
+        <Link 
+          href={'/terms'}>
+          Terms of Service
+        </Link>
+        <Link href={'/privacy'}>
+          Privacy Policy
+        </Link>
       </div>
-    </div> 
+    </div>
   )
 }
 
