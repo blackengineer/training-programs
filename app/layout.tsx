62d73f8d10654b92a1d0b8c086481a6b6dc312f5
Jimmy <jimmlindsey@gmail.com>
2024-01-31 16:59:49 -0800

    update layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'How to train for',
   description: 'How to train for anything',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
 }
 
 export default async function RootLayout({
