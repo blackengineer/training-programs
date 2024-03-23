66acd5fb8debd685c8a2f2c8147a8c41a590953e
Jimmy <jimmlindsey@gmail.com>
2024-03-23 16:47:10 -0700

    update static navbar

--- app/(static)/_components/StaticNavbar.tsx
+++ app/(static)/_components/StaticNavbar.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import Logo from '@/components/Logo'
+import { Button } from '@/components/ui/Button'
+import { LogOut } from 'lucide-react'
 import { Montserrat } from 'next/font/google'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -18,6 +20,14 @@ const StaticNavbar = async () => {
           <Logo />
         </div>
       </Link>
+      <div className='flex gap-x-2 ml-auto'>
+        <Link href={'/'}>
+          <Button variant={'ghost'} className='mt-0.5'>
+              <LogOut className='h-4 w-4 mr-2'/>
+              Home
+            </Button>
+        </Link> 
+      </div>
     </nav>
   )
 }
